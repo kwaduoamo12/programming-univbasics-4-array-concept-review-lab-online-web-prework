baa8983d3101228e8d652426f3f77924c864dbb5
Samuel Owusu <kwadwoamo10@gmail.com>
2019-09-02 22:18:26 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,15 @@
-def find_element_index(array, value_to_find)
+def 6.times do(array, value_to_find)
   # Add your solution here
+  puts array = [1,2,3,4,5,6]
+  puts values_to_find
 end
 
-def find_max_value(array)
+def 6.times do(food)
   # Add your solution here
+  puts food
 end
 
 def find_min_value(array)
   # Add your solution here
+  puts 
 end
