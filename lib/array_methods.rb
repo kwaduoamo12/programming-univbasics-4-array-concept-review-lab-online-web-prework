7d12e4be3839601403eacefc423f0ca9d9857483
Samuel Owusu <kwadwoamo10@gmail.com>
2019-09-03 22:08:30 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,15 +1,29 @@
-def 6.times do(array, value_to_find)
+def find_element_index(array, value_to_find)
   # Add your solution here
-  puts array = [1,2,3,4,5,6]
-  puts values_to_find
-end
+  #while i = 0 find_element_index (array)
+# puts find_element_index |array|
+# find_element_index += 1 
+i = 0 
+while i < array.length do 
+  if array[i] == value_to_find
+    return i 
+  end 
+  i += 1 
+ end 
+ nil 
+ end
+ 
+#while find_element_index(value_to_find)do
+  #puts find_element_index |value_to_find|
+  
+#end 
 
-def 6.times do(food)
+def find_max_value(array)
   # Add your solution here
-  puts food
+ # puts find_max_value (array)
 end
 
 def find_min_value(array)
   # Add your solution here
-  puts 
+ # puts find_min_value
 end
