376bbea462e4e83905178aab9ff6992d85094262
Samuel Owusu <kwadwoamo10@gmail.com>
2019-09-05 18:45:19 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -10,7 +10,7 @@ while i < array.length do
  end
  
 def find_max_value(array)
- i += 0
+ i = 0
   while i < array.length 
   if array[i] == find_max_value
     return i 
@@ -19,7 +19,7 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-  i += 0 
+  i = 0 
  while i < array.length 
   if array[i] == find_min_value
     return i 
